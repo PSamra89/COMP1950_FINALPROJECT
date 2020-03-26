3ac69923a59a11001ea90f736e6e8f121c4afa74
Pavan <psamra14@my.bcit.ca>
2020-03-25 21:20:00 -0700

    small fix

--- script/mobile.js
+++ script/mobile.js
@@ -1,29 +1,29 @@
 $(document).ready(function() {
-  window.onresize = displayWindowSize;
-  window.onload = displayWindowSize;
+    window.onresize = displayWindowSize;
+    window.onload = displayWindowSize;
 
-  function displayWindowSize() {
-    myWidth = window.innerWidth;
-    myHeight = window.innerHeight;
-    // your size calculation code here
+    function displayWindowSize() {
+        myWidth = window.innerWidth;
+        myHeight = window.innerHeight;
+        // your size calculation code here
 
-    if (myWidth < 980) {
-      {
-        var btn = document.getElementsByClassName("entypo-menu")[0];
-        btn.addEventListener("click", function() {
-          var temp = $(".mobile-nav").css("display");
-          console.log(temp);
-          if (temp == "block") {
-            console.log("changin to none");
+        if (myWidth < 980) {
+            {
+                var btn = document.getElementsByClassName("entypo-menu")[0];
+                btn.addEventListener("click", function() {
+                    var temp = $(".mobile-nav").css("display");
+                    console.log(temp);
+                    if (temp == "block") {
+                        console.log("changin to none");
+                        $(".mobile-nav").css("display", "none");
+                    } else if (temp == "none") {
+                        console.log("changin to block");
+                        $(".mobile-nav").css("display", "block");
+                    }
+                });
+            }
+        } else {
             $(".mobile-nav").css("display", "none");
-          } else if (temp == "none") {
-            console.log("changin to block");
-            $(".mobile-nav").css("display", "block");
-          }
-        });
-      }
-    } else {
-      $(".mobile-nav").css("display", "none");
+        }
     }
-  }
 });
