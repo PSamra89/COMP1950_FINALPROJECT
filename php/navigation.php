c9d5c4654e7e95d43c57d613b748488860af5a15
Pavan <psamra14@my.bcit.ca>
2020-03-25 23:53:36 -0700

    fixes

--- php/navigation.php
+++ php/navigation.php
@@ -1,31 +1,32 @@
 <div id="nav-wrap">
-        <nav>
-            <ul class="nav_links">
-                <li id="home">
-                    <a href="../pages/homepage.html">
-                        <span class="entypo-home" data-text="home"></span>
-                    </a>
-                </li>
-                <li>
-                    <a href="../pages/lecture.html">Lecture</a>
-                </li>
-                <li>
-                    <a href="../pages/about.html">About</a>
-                </li>
-                <li>
-                    <form>
-                        <input type="search" placeholder="Search.." name="search" />
-                        <button type="submit">
-                <span class="entypo-search" data-text="search"></span>
-              </button>
-                    </form>
-                </li>
-                <li>
-                    <a id="Introduction" href="../pages/index.html">
-                        <span class="entypo-back" data-text="back-in-time"></span>
-                    </a>
-                </li>
+  <nav>
+      <ul class="nav_links">
+          <li id="home">
+              <a href="../pages/homepage.html">
+                  <span class="entypo-home" data-text="home"></span>
+                  <span class="entypo-menu" data-text="home"></span>
+              </a>
+          </li>
+          <li class="lec">
+              <a href="../pages/lecture.html">Lecture</a>
+          </li>
+          <li class="lec">
+              <a href="../pages/about.html">About</a>
+          </li>
+          <li class="lec">
+              <form>
+                  <input type="search" placeholder="Search.." name="search" />
+                  <button type="submit">
+          <span class="entypo-search" data-text="search"></span>
+        </button>
+              </form>
+          </li>
+          <li>
+              <a id="Introduction" href="../pages/index.html">
+                  <span class="entypo-back" data-text="back-in-time"></span>
+              </a>
+          </li>
 
-            </ul>
-        </nav>
+      </ul>
+  </nav>
 </div>
