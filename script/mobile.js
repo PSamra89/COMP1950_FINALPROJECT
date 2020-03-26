9d2cd510aa444ac0de7844a5858be03e9f0f204e
Tenzin <phur1987@gmail.com>
2020-03-25 23:48:34 -0700

    done refreshing

--- script/mobile.js
+++ script/mobile.js
@@ -1,13 +1,15 @@
-$(document).ready(function() {});
-var btn = document.getElementsByClassName("entypo-menu")[0];
-btn.addEventListener("click", function() {
-  var temp = $(".mobile-nav").css("display");
-  console.log(temp);
-  if (temp == "block") {
-    console.log("changin to none");
-    $(".mobile-nav").css("display", "none");
-  } else if (temp == "none") {
-    console.log("changin to block");
-    $(".mobile-nav").css("display", "block");
-  }
+$(document).ready(function(evt) {
+  var btn = document.getElementsByClassName("entypo-menu")[0];
+  btn.addEventListener("click", function(evt) {
+    evt.preventDefault();
+    var temp = $(".mobile-nav").css("display");
+    console.log(temp);
+    if (temp == "block") {
+      console.log("changin to none");
+      $(".mobile-nav").css("display", "none");
+    } else if (temp == "none") {
+      console.log("changin to block");
+      $(".mobile-nav").css("display", "block");
+    }
+  });
 });
